84674c91d51085ffb3b1553adfe9554a7485aba7
Gonzalo Moreno <gonzalo.moreno@flexxus.com.ar>
2024-03-12 22:08:29 -0300

    first commit

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+//const GIPHY_API_KEY = "8He43U8FKg33iFajQbVZkSaZSVYHJMhu";
+
+@Injectable({ providedIn: 'root' })
+export class GifsService {
+  constructor(private http: HttpClient) {}
+
+  public gifList: Gif[] = [];
+
+  private _tagsHistory: string[] = [];
+  private apiKey: string = '8He43U8FKg33iFajQbVZkSaZSVYHJMhu';
+  private serviceUrl: string = `https://api.giphy.com/v1/gifs`;
+
+  get tagsHistory() {
+    return [...this._tagsHistory];
+  }
+
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if (this._tagsHistory.includes(tag))
+      this._tagsHistory = this._tagsHistory.filter((olTag) => olTag !== tag);
+
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
+  }
+
+  searchTag(tag: string): void {
+    if (!tag) return;
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+
+    this.http
+      .get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe((resp) => {
+        this.gifList = resp.data;
+      });
+  }
+}
